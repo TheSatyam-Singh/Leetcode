04c9e522fedf6f715ea4ec05a5b4669885a48467
Satyam Singh <129391643+TheSatyam-Singh@users.noreply.github.com>
2026-07-20 11:23:49 +0530

    Create 4057-TotalWavinessOfNumbersInRangeI

---
+++ 4057-TotalWavinessOfNumbersInRangeI/4057-TotalWavinessOfNumbersInRangeI.cpp
@@ -0,0 +1,16 @@
+// Last updated: 20/07/2026, 11:23:48
+class Solution {
+public:
+    int totalWaviness(int num1, int num2) {
+        int ans=0;
+        for(int i=num1;i<=num2;i++){
+            string s=to_string(i);
+            for(int j=1;j<s.size()-1;j++){
+                if((s[j]>s[j+1] && s[j]>s[j-1])||(s[j]<s[j-1] &&s [j]<s[j+1])){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
