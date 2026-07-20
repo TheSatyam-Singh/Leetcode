577aab95bcdcd131e8840ce881d6912e7afef36a
Satyam Singh <129391643+TheSatyam-Singh@users.noreply.github.com>
2026-07-20 11:24:26 +0530

    Create 3952-TrionicArrayI

---
+++ 3952-TrionicArrayI/3952-TrionicArrayI.cpp
@@ -0,0 +1,20 @@
+// Last updated: 20/07/2026, 11:24:26
+class Solution {
+public:
+    bool isTrionic(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 4) return false;
+
+        int i = 1;
+
+        while (i < n && nums[i] > nums[i - 1]) i++;
+        if (i == 1 || i == n) return false;
+
+        while (i < n && nums[i] < nums[i - 1]) i++;
+        if (i == n) return false;
+
+        while (i < n && nums[i] > nums[i - 1]) i++;
+
+        return i == n;
+    }
+};
