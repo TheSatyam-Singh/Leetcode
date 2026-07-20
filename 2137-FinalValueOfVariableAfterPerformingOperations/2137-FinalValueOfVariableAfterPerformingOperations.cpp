03689ee239cc1560fd813dc39aec19782b19935a
Satyam Singh <129391643+TheSatyam-Singh@users.noreply.github.com>
2026-07-20 11:26:35 +0530

    Create 2137-FinalValueOfVariableAfterPerformingOperations

---
+++ 2137-FinalValueOfVariableAfterPerformingOperations/2137-FinalValueOfVariableAfterPerformingOperations.cpp
@@ -0,0 +1,16 @@
+// Last updated: 20/07/2026, 11:26:35
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int X=0;
+        for (int i=0;i<operations.size();i++){
+            if (operations[i]=="++X" || operations[i]=="X++"){
+                X++;
+            }
+            if (operations[i]=="--X" || operations[i]=="X--"){
+                X--;
+            }
+        }
+        return X;
+    }
+};
