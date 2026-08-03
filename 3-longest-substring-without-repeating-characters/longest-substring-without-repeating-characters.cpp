998ba6dc4551425a0a4d79cc6e656d10c726ad83
Satyam Singh <129391643+TheSatyam-Singh@users.noreply.github.com>
2026-08-03 16:02:00 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (9.73%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -14,18 +14,6 @@ public:
             right++;
         }
         return ans;
-        /*vector<char>ans;
-        int l=0;
-        int mx=0;
-        for(int i=0;i<s.size();i++){
-            while (find(ans.begin(),ans.end(), s[i]) != ans.end()) {
-                ans.erase(find(ans.begin(),ans.end(),s[l]));
-                l++;
-            }
-            ans.push_back(s[i]);
-            mx=max(mx,i-l+1);
-        }
-        return mx;*/
     }
 };
 
