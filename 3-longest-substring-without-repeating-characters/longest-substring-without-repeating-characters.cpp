83ed253e91a796cbf660980236a1509cc0306385
Satyam Singh <129391643+TheSatyam-Singh@users.noreply.github.com>
2026-08-03 15:59:29 +0530

    Time: 7 ms (64.58%) | Memory: 19 MB (10.97%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int> lastSeen(256, -1); // Tracks the last index where each character appeared
-        int maxLength = 0;
-        int left = 0; // Left pointer of the sliding window
-
-        for (int right = 0; right < s.length(); right++) {
-            char currChar = s[right];
-
-            // If the character was seen inside the current window, move the left pointer
-            if (lastSeen[currChar] >= left) {
-                left = lastSeen[currChar] + 1;
+        vector<int>length(256,-1);
+        int left=0;
+        int right=0;
+        int ans=0;
+        while(right<s.size()){
+            if(length[s[right]] != -1){
+                left=max(left,length[s[right]]+1);
             }
-
-            // Update last seen position of current character
-            lastSeen[currChar] = right;
-
-            // Calculate max window size
-            maxLength = max(maxLength, right - left + 1);
+            length[s[right]]=right;
+            ans=max(ans,right-left+1);
+            right++;
         }
-
-        return maxLength;
+        return ans;
+        /*vector<char>ans;
+        int l=0;
+        int mx=0;
+        for(int i=0;i<s.size();i++){
+            while (find(ans.begin(),ans.end(), s[i]) != ans.end()) {
+                ans.erase(find(ans.begin(),ans.end(),s[l]));
+                l++;
+            }
+            ans.push_back(s[i]);
+            mx=max(mx,i-l+1);
+        }
+        return mx;*/
     }
 };
+
+
