1601330e3ac9289c3682bdab2e9603ca547081f1
Satyam Singh <129391643+TheSatyam-Singh@users.noreply.github.com>
2026-08-14 11:24:44 +0530

    Time: 3 ms (89.63%) | Memory: 56.8 MB (30.78%) - LeetSync

--- 355-design-twitter/design-twitter.cpp
+++ 355-design-twitter/design-twitter.cpp
@@ -1,72 +1,50 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Twitter {
-private:
-
-    // user -> list of {time, tweetId}
-    unordered_map<int, vector<pair<int, int>>> tweets;
-
-    // follower -> set of people they follow
-    unordered_map<int, unordered_set<int>> following;
-
-    int timer = 0;
-
 public:
-
+    int time=0;
+    unordered_map<int,vector<pair<int,int>>>tweet;
+    unordered_map<int, unordered_set<int>>following;
     Twitter() {
+        
     }
-
+    
     void postTweet(int userId, int tweetId) {
-
-        tweets[userId].push_back({timer, tweetId});
-
-        timer++;
+        tweet[userId].push_back({time,tweetId});
+        time++;
     }
-
+    
     vector<int> getNewsFeed(int userId) {
-
-        vector<int> ans;
-
-        // Max heap:
-        // {time, tweetId}
-        priority_queue<pair<int, int>> pq;
-
-        // User's own tweets
-        for (auto tweet : tweets[userId]) {
-            pq.push(tweet);
+        vector<int>ans;
+        vector<pair<int,int>>v;
+        for(auto it:tweet[userId]){
+            v.push_back(it);
         }
-
-        // Tweets of followed users
-        for (int followee : following[userId]) {
-
-            for (auto tweet : tweets[followee]) {
-                pq.push(tweet);
+        for(auto it:following[userId]){
+            for(auto i:tweet[it]){
+                v.push_back(i);
             }
         }
-
-        // Take 10 newest tweets
-        while (!pq.empty() && ans.size() < 10) {
-
-            auto [time, tweetId] = pq.top();
-            pq.pop();
-
-            ans.push_back(tweetId);
+        sort(v.rbegin(),v.rend());
+        int n=v.size();
+        for(int i=0;i<min(n,10);i++){
+            ans.push_back(v[i].second);
         }
-
         return ans;
     }
-
+    
     void follow(int followerId, int followeeId) {
-
-        if (followerId == followeeId)
-            return;
-
         following[followerId].insert(followeeId);
     }
-
+    
     void unfollow(int followerId, int followeeId) {
-
         following[followerId].erase(followeeId);
     }
 };
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter* obj = new Twitter();
+ * obj->postTweet(userId,tweetId);
+ * vector<int> param_2 = obj->getNewsFeed(userId);
+ * obj->follow(followerId,followeeId);
+ * obj->unfollow(followerId,followeeId);
+ */
