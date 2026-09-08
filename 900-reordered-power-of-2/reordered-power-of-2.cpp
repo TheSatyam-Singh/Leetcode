15a52e2a1df56f0d9889daef8aad25b5fb95fca1
Satyam Singh <129391643+TheSatyam-Singh@users.noreply.github.com>
2026-09-08 21:21:05 +0530

    Time: 1 ms (34.68%) | Memory: 9 MB (42.26%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool reorderedPowerOf2(int n) {
-        string s=to_string(n);
-        sort(s.begin(),s.end());
-        for(int i=0;i<=30;i++){
-            int a=1<<i;
-            string t=to_string(a);
-            sort(t.begin(),t.end());
-            if(s==t){
+        string s = to_string(n);
+        sort(s.begin(), s.end());
+        for (int i = 0; i <= 30; i++) {
+            int a = 1 << i;
+            string t = to_string(a);
+            sort(t.begin(), t.end());
+            if (s == t) {
                 return true;
             }
         }
