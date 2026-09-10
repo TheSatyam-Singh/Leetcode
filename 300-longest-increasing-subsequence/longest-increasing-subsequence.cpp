0c0eb16ef06d7167c51d6222a7230c75fb459d4e
Satyam Singh <129391643+TheSatyam-Singh@users.noreply.github.com>
2026-09-10 12:47:58 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (77.75%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
-    int solve(vector<int>&nums, int idx,int prev,vector<vector<int>>& dp){
-        if(idx>=nums.size()){
-            return 0;
-        }
-        if (dp[idx][prev + 1] != -1) {
-            return dp[idx][prev + 1];
-        }
+    // int solve(vector<int>&nums, int idx,int prev,vector<vector<int>>& dp){
+    //     if(idx>=nums.size()){
+    //         return 0;
+    //     }
+    //     if (dp[idx][prev + 1] != -1) {
+    //         return dp[idx][prev + 1];
+    //     }
 
-        int ntake=solve(nums,idx+1,prev,dp);
-        int take=0;
-        if(prev==-1 || nums[idx]>nums[prev]){
-            take=1+solve(nums,idx+1,idx,dp);
-        }
-        return dp[idx][prev + 1] = max(take, ntake);
-    }
+    //     int ntake=solve(nums,idx+1,prev,dp);
+    //     int take=0;
+    //     if(prev==-1 || nums[idx]>nums[prev]){
+    //         take=1+solve(nums,idx+1,idx,dp);
+    //     }
+    //     return dp[idx][prev + 1] = max(take, ntake);
+    // }
+    // int lengthOfLIS(vector<int>& nums) {
+    //     int n=nums.size();
+    //     vector<vector<int>>dp(n,vector<int>(n+1,-1));
+    //     return solve(nums,0,-1,dp);
+    // }
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return solve(nums,0,-1,dp);
+        vector<int>ans;
+        for(int i:nums){
+            auto it=lower_bound(ans.begin(),ans.end(),i);
+            if(it==ans.end()){
+                ans.push_back(i);
+            }else{
+                *it=i;
+            }
+        }
+        return ans.size();
     }
 };
