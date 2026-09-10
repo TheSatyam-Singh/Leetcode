0fedb8c145da7e3c7d92e38dafdf4bb6fc9a14ea
Satyam Singh <129391643+TheSatyam-Singh@users.noreply.github.com>
2026-09-10 11:50:50 +0530

    Time: 442 ms (15.45%) | Memory: 8.6 MB (59.53%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    int solve(string s,int l, int r,vector<vector<int>> &dp){
-        if(l>=r){
-            return true;
-        }
-        if (dp[l][r]!=-1) {
-            return dp[l][r];
-        }
-        if (s[l]!=s[r]) {
-            return dp[l][r]=false;
-        }
-        return dp[l][r]=solve(s,l+1,r-1,dp);
-    }
-    int countSubstrings(string s) {
-        int n=s.size();
-        int ans=0;
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(solve(s,i,j,dp)){
-                    ans++;
-                }
-            }
-        }
-        return ans;
-    }
+    // int solve(string s,int l, int r,vector<vector<int>> &dp){
+    //     if(l>=r){
+    //         return true;
+    //     }
+    //     if (dp[l][r]!=-1) {
+    //         return dp[l][r];
+    //     }
+    //     if (s[l]!=s[r]) {
+    //         return dp[l][r]=false;
+    //     }
+    //     return dp[l][r]=solve(s,l+1,r-1,dp);
+    // }
+    // int countSubstrings(string s) {
+    //     int n=s.size();
+    //     int ans=0;
+    //     vector<vector<int>> dp(n, vector<int>(n, -1));
+    //     for(int i=0;i<n;i++){
+    //         for(int j=i;j<n;j++){
+    //             if(solve(s,i,j,dp)){
+    //                 ans++;
+    //             }
+    //         }
+    //     }
+    //     return ans;
+    // }
 
     // ---------recursion--------------
     // int solve(string s,int l, int r){
@@ -50,27 +50,27 @@ public:
     // }
 
     // ----------brute force----------
-    // int countSubstrings(string s) {
-    //     int n = s.size();
-    //     int ans=0;
-    //     for(int i=0;i<n;i++){
-    //         for(int j=i;j<n;j++){
-    //             int l=i;
-    //             int r=j;
-    //             bool palin=true;
-    //             while(l<r){
-    //                 if(s[l]!=s[r]){
-    //                     palin=false;
-    //                     break;
-    //                 }
-    //                 l++;
-    //                 r--;
-    //             }
-    //             if(palin){
-    //                 ans++;
-    //             }
-    //         }
-    //     }
-    //     return ans;
-    // }
+    int countSubstrings(string s) {
+        int n = s.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int l=i;
+                int r=j;
+                bool palin=true;
+                while(l<r){
+                    if(s[l]!=s[r]){
+                        palin=false;
+                        break;
+                    }
+                    l++;
+                    r--;
+                }
+                if(palin){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
 };
